27145f53894df3488949625ec4922760a790bd4a
PinghuaZhuang <zphua2016@gmail.com>
2018-08-22 09:00:37 +0800

    Update: 完善

--- src/js/Shield/colors.js
+++ src/js/Shield/colors.js
@@ -2,7 +2,7 @@ define( [], function () {
     'use strict';
 
     return {
-        area: [ '#e60012', '#577bcf', '#ffc924' ],
+        area: [ '#e60012', '#ffc924', '#577bcf' ],
         bg: [ '#577bcf', '#95c3f5', '#ffc929' ]
     };
 } );
